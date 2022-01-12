997cbf24667c9d900d6c8d343123e4d6f97c2c04
melp <huzi.master@outlook.com>
2022-01-12 11:08:34 +0800

    add: day2 puzzle2.

--- src/day2/main.lisp
+++ src/day2/main.lisp
@@ -1,6 +1,7 @@
 (defpackage :aoc2021.day2
   (:use :cl)
-  (:export :puzzle1)
+  (:export :puzzle1
+           :puzzle2)
   (:import-from :aoc2021.util
                 :parse-input
                 :parse-int))
@@ -9,6 +10,7 @@
 
 (defparameter +depth+ 0)
 (defparameter +distance+ 0)
+(defparameter +aim+ 0)
 
 ;; oop version
 ;; TODO: functionnal version
@@ -21,8 +23,13 @@
 (defclass down-command (command) ())
 (defclass forward-command (command) ())
 
+(defclass up-command2 (command) ())
+(defclass down-command2 (command) ())
+(defclass forward-command2 (command) ())
+
 (defgeneric execute (obj))
 
+;; puzzle1
 (defmethod execute ((obj up-command))
   (setf +depth+ (- +depth+
                    (value obj))))
@@ -35,6 +42,22 @@
   (setf +distance+ (+ (value obj)
                       +distance+)))
 
+;; puzzle2
+(defmethod execute ((obj up-command2))
+  (setf +aim+ (- +aim+
+                 (value obj))))
+
+(defmethod execute ((obj down-command2))
+  (setf +aim+ (+ (value obj)
+                   +aim+)))
+
+(defmethod execute ((obj forward-command2))
+  (setf +distance+ (+ (value obj)
+                      +distance+))
+  (setf +depth+ (+ (* +aim+ (value obj))
+                   +depth+)))
+
+
 (defun parse-comand (line)
   (let* ((idx (search " " line))
          (name (subseq line 0 idx))
@@ -43,6 +66,16 @@
       ((string= name "up") (make-instance 'up-command :value value))
       ((string= name "down") (make-instance 'down-command :value value))
       ((string= name "forward") (make-instance 'forward-command :value value)))))
+
+(defun parse-comand2 (line)
+  (let* ((idx (search " " line))
+         (name (subseq line 0 idx))
+         (value (parse-int (subseq line (incf idx)))))
+    (cond
+      ((string= name "up") (make-instance 'up-command2 :value value))
+      ((string= name "down") (make-instance 'down-command2 :value value))
+      ((string= name "forward") (make-instance 'forward-command2 :value value)))))
+
 
 (defun execute-commands (commands)
   (cond
@@ -53,6 +86,12 @@
 
 (defun puzzle1 ()
   (let* ((commands (parse-input "input1.txt" #'parse-comand)))
+    (execute-commands commands)
+    (* +depth+
+       +distance+)))
+
+(defun puzzle2 ()
+  (let* ((commands (parse-input "input1.txt" #'parse-comand2)))
     (execute-commands commands)
     (* +depth+
        +distance+)))
