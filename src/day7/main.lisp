93202904f1dd5995b6909cc254f97fecf539e4fe
melp <huzi.master@outlook.com>
2022-01-21 17:07:48 +0800

    add: day7 puzzle2.

--- src/day7/main.lisp
+++ src/day7/main.lisp
@@ -7,10 +7,13 @@
 (defun input-to-nums (line)
   (read-from-string (concatenate 'string "(" (substitute #\Space #\, line) ")")))
 
-(defun fules (nums)
+(defun fules (nums &key (c #'(lambda (x) x)))
   (loop for i from 0 to (apply #'max nums)
         collect (cons i (apply #'+ (loop for num in nums
-                                         collect (abs (- num i)))))))
+                                         collect (funcall c (abs (- num i))))))))
+
+(defun step-to-cost (num)
+  (loop for i from 1 to num sum i))
 
 (defun least (fus)
   (let* ((fs (mapcar #'cdr fus))
@@ -24,3 +27,8 @@
   (with-open-file (in "input2.txt")
     (let* ((nums (input-to-nums (read-line in nil))))
       (least (fules nums)))))
+
+(defun puzzle2 ()
+  (with-open-file (in "input2.txt")
+    (let* ((nums (input-to-nums (read-line in nil))))
+      (least (fules nums :c #'step-to-cost)))))
