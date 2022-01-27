7a9167b982457117f8ceede3c044566dac016db3
melp <huzi.master@outlook.com>
2022-01-27 22:19:37 +0800

    add: day10 puzzle1.

--- src/day10/main.rkt
+++ src/day10/main.rkt
@@ -1,18 +1,17 @@
 #lang racket
 
-(define chunk-start '(#\( #\[ #\{ #\<))
-(define chunk-end '(#\) #\] #\} #\>))
-
 (define start-to-end '((#\( . #\))
                        (#\[ . #\])
                        (#\{ . #\})
                        (#\< . #\>)))
 
+(define scores '((#\) . 3)
+                 (#\] . 57)
+                 (#\} . 1197)
+                 (#\> . 25137)))
+
 (define (chunk-start? ch)
   (assoc ch start-to-end))
-
-(define (chunk-end? ch)
-  (member ch chunk-end))
 
 (define (read-chunk line)
   (cond
@@ -21,28 +20,60 @@
      (read-chunkk (string-ref line 0) line 1)]))
 
 ;; [<>({}){}[([])<>]]
+;; {([(<{}[<>[]}>{[]{[(<()>
 (define (read-chunkk start line pos)
   (define len (string-length line))
   (cond
-    [(<= len pos) 'incomplete]
+    [(<= len pos) (list 'incomplete)]
     [(char=? (cdr (assoc start start-to-end))
-             (string-ref line pos)) (add1 pos)]
-    [(chunk-start? (string-ref line pos)) (let loop ([new-pos (read-chunkk (string-ref line pos)
-                                                                           line
-                                                                           (add1 pos))])
-                                            (cond
-                                              [(symbol? new-pos) new-pos]
-                                              [(<= len pos) 'incomplete]
-                                              [(char=? (string-ref line new-pos)
-                                                       (cdr (assoc start start-to-end))) (add1 new-pos)]
-                                              [(chunk-start? (string-ref line new-pos)) (loop (read-chunkk (string-ref line new-pos)
-                                                                                                           line
-                                                                                                           (add1 new-pos)))]))]
-    [(chunk-end? (string-ref line pos)) 'corrupted]))
+             (string-ref line pos))
+     (add1 pos)]
+    [(chunk-start? (string-ref line pos))
+     (let loop ([new-pos (read-chunkk (string-ref line pos)
+                                      line
+                                      (add1 pos))])
+       (cond
+         [(list? new-pos) new-pos]
+         [(<= len new-pos) (list 'incomplete)]
+         [(char=? (string-ref line new-pos)
+                  (cdr (assoc start start-to-end)))
+          (add1 new-pos)]
+         [(chunk-start? (string-ref line new-pos))
+          (loop (read-chunkk (string-ref line new-pos)
+                             line
+                             (add1 new-pos)))]
+         [else (list 'corrupted
+                     (cdr (assoc start start-to-end))
+                     (string-ref line new-pos)
+                     new-pos) ]))]
+    [else
+     (list 'corrupted
+           (cdr (assoc start start-to-end))
+           (string-ref line pos)
+           pos)]))
 
 (module+ test
   (require rackunit)
   (read-chunk "()()")
   (read-chunk "[<>({}){}[(])<>]]")
+  (read-chunk "{([(<{}[<>[]}>{[]{[(<()>")
   )
 
+(define (puzzle1 filename)
+
+  (define corruptes '())
+  (call-with-input-file filename
+    (lambda (in)
+      (for ([line (in-lines in)])
+        (let ([res (read-chunk line)])
+          (cond
+            [(and (list? res) (equal? (car res) 'corrupted))
+             (set! corruptes (append corruptes (list (caddr res))))])))))
+  (for/sum ([c (in-list corruptes)])
+    (cdr (assoc c scores)))
+  )
+
+(module+ test
+  (puzzle1 "input1.txt")
+  (puzzle1 "input2.txt")
+  )
