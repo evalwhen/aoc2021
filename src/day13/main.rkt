fc491999a14af97089b3e8cd44e1b4c17071a8a7
melp <huzi.master@outlook.com>
2022-02-04 09:53:30 +0800

    add: day13 puzzle1.

--- src/day13/main.rkt
+++ src/day13/main.rkt
@@ -6,17 +6,50 @@
                         (string->number v2))]))
 
 (define (parse-data filename)
+  (define dots (make-hash))
+
   (call-with-input-file filename
     (lambda (in)
-      (for/fold ([dots (hash)])
-                ([line (in-lines in)]
+      (for ([line (in-lines in)]
                  #:break (string=? line ""))
-        (hash-set dots (line-to-dots line) #t)))))
+        (hash-set! dots (line-to-dots line) #t))))
+  dots)
 
 (define trans-fold
   (lambda (at)
     (lambda (v)
       (- (* 2 at) v))))
 
+(define (fold-up dots at)
+  (define res (make-hash))
+
+  (define converter (trans-fold at))
+  (for* ([x (in-range 0 4000)]
+         [y (in-range 0 at)])
+    (when (hash-ref dots (cons x (converter y)) #f)
+      (hash-set! res (cons x y) #t))
+    (when (hash-ref dots (cons x y) #f)
+      (hash-set! res (cons x y) #t)))
+  res)
+
+;; todo, remove dup
+(define (fold-left dots at)
+  (define res (make-hash))
+
+  (define converter (trans-fold at))
+
+  (for* ([x (in-range 0 at)]
+         [y (in-range 0 3000)])
+    (when (hash-ref dots (cons (converter x) y) #f)
+      (hash-set! res (cons x y) #t))
+    (when (hash-ref dots (cons x y) #f)
+      (hash-set! res (cons x y) #t)))
+  res
+  )
+
 (module+ test
-  ((trans-fold 7) 0))
+  (hash-count (parse-data "input2.txt"))
+  ;; ((trans-fold 7) 0)
+  (hash-count (fold-up (parse-data "input1.txt") 7))
+  (hash-count (fold-left (parse-data "input2.txt") 655))
+  )
