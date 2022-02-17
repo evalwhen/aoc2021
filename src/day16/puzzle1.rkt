b1a0afd079b9b5ecc98a4004d1712f7b273cd6a9
melp <huzi.master@outlook.com>
2022-02-17 20:16:36 +0800

    day16: more.

--- src/day16/puzzle1.rkt
+++ src/day16/puzzle1.rkt
@@ -1,6 +1,14 @@
 #lang racket
 
-(define (parse-literal input)
+(struct result (ver body remaining))
+
+(define (parse-packet input res)
+  null)
+
+(define (parse-op input res)
+  null)
+
+(define (parse-literal ver input)
   (define  (stop-p group)
     (char=? (string-ref group 0) #\0))
 
@@ -9,10 +17,35 @@
            [part (substring group 1)])
       ;; (println group)
       (cond
-        [(stop-p group) (string-append out part)]
+        [(stop-p group) (result ver (string-append out part) (substring in 5))]
         [else (loop (substring in 5)
                     (string-append out part))]))))
 
+(define (parse-op-by-len input len res)
+  (case (packet-type input)
+    [('literal) (let ([r (parse-literal input)])
+                  (cond
+                    [(= len (+ 6 (body-len r))) (cons r res)]
+                    [else (parse-packet (result-remaining r) (cons r res))]))]
+    [('op) (let ([r (parse-op input res)]
+                 [header (op-header input)])
+             (cond
+               [(= len (+ (string-length header)
+                          (body-len r)))
+                (cons r res)]
+               [else (parse-packet (result-remaining r) (cosn r res))]))]))
+
+(define (body-len r)
+  0)
+
+(define (packet-type input)
+  'literal)
+
+(define (op-type input)
+  'len)
+
+(define (op-header input)
+  "")
 (define (bits-to-decimal bits)
   (define last-pos (- (string-length bits) 1))
 
